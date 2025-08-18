a510f0e00d04d57107bff1d70445bc353a25a3a4
HaseethVardhan <haseethvardhan01@gmail.com>
2025-08-18 14:34:54 +0530

    finished navbar and added fonts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Navbar } from "./components/Navbar"
+
 function App() {
 
   return (
-    <>
-    </>
+    <div className="w-screen h-screen bg-[#161616] py-6">
+      <Navbar />
+    </div>
   )
 }
 
